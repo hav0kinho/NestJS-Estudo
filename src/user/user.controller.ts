c061682a4cbd41927e30aefe7e654f20fb41bb74
hav0kinho <ruallysonfelype@gmail.com>
2024-06-11 08:14:31 -0300

    15 | 16 - Criação das rotas de Post e Get

---
+++ src/user/user.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+
+@Controller("users")
+export class UserController{
+    @Post()
+    async create(@Body() body){
+        return {body};
+    }
+
+    @Get()
+    async list(){
+        return {users:[]}
+    }
+
+    @Get(':id')
+    async show(@Param() param){
+        return {user:{}, param}
+    }
+}
