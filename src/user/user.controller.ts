6123117d954331ab3103132311acd5afd90df7d0
hav0kinho <ruallysonfelype@gmail.com>
2024-06-11 08:22:57 -0300

    17  | 18 - Criação das rotas de Update e Delete

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
 
 @Controller("users")
 export class UserController{
@@ -15,5 +15,30 @@ export class UserController{
     @Get(':id')
     async show(@Param() param){
         return {user:{}, param}
+    }
+
+    @Put(":id")
+    async update(@Body() body, @Param() params){
+        return{
+            method: 'put',
+            body,
+            params
+        }
+    }
+
+    @Patch(":id")
+    async updatePartial(@Body() body, @Param() params){
+        return{
+            method: 'patch',
+            body,
+            params
+        }
+    }
+
+    @Delete(":id")
+    async delete(@Param() params){
+        return{
+            params
+        }
     }
 }
