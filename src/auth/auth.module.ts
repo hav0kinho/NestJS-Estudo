bbdaa3091d5540fe10c6ed4303ac888cfe414769
hav0kinho <ruallysonfelype@gmail.com>
2024-07-08 12:26:57 -0300

    38 - AuthController | Criando o modulo de autenticação inicial (Ainda falta implementar as funcionalidades do token JWT)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  controllers: [AuthController],
   imports: [
     JwtModule.register({
       secret: "3+|M5/OJ0>k/7syp!!.zMk'H£0m]gd$I",
     }),
+    UserModule,
+    PrismaModule,
   ],
 })
 export class AuthModule {}
