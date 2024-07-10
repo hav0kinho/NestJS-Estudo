e5b360905e436539b8bc1899ca77ae34067f36fb
hav0kinho <ruallysonfelype@gmail.com>
2024-07-10 15:44:18 -0300

    Adicionando comentários ao Decorator user.decorator

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -4,6 +4,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 
+// Esse decorator deve ser usado em conjunto com o auth.guard! Ele recebe alguma informação passada como parâmetro (email, id, user, etc) e então retorna apenas essa informação. Também pode retornar todos os dados do usuário caso não seja passado o parâmetro.
 export const User = createParamDecorator(
   (filter: string, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
